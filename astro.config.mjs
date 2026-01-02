6987e662a19f7907719cc135974569ceab6e9c11
yuru36 <cliff.asura@gmail.com>
2026-01-02 20:38:10 +0900

    astro.config直す

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,15 +3,7 @@ import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 
-import cloudflare from "@astrojs/cloudflare";
-
-// https://astro.build/config
 export default defineConfig({
-	site: "https://example.com",
 	integrations: [mdx(), sitemap()],
-	adapter: cloudflare({
-		platformProxy: {
-			enabled: true,
-		},
-	}),
+	output: "static",
 });
