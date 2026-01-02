13ea8eb82a7ad6254ff69e32c04c4cbe942bc6b8
yuru36 <cliff.asura@gmail.com>
2026-01-02 21:45:00 +0900

    tailwind入れる

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,13 @@ import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 
+import tailwindcss from "@tailwindcss/vite";
+
 export default defineConfig({
-	integrations: [mdx(), sitemap()],
-	output: "static",
+  integrations: [mdx(), sitemap()],
+  output: "static",
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
