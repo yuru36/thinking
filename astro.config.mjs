6eeb6267eaaeb58b684f98048bd032aa8a6be54e
yuru36 <cliff.asura@gmail.com>
2026-01-02 21:52:37 +0900

    site URL追加

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,7 @@ import sitemap from "@astrojs/sitemap";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
+  site: "https://meganegirl-architecture.dev",
   integrations: [mdx(), sitemap()],
   output: "static",
 
